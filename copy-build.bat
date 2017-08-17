2853120d59a1a329bcffa233757ece2336681792
mikemahedy <mahedy@vt.edu>
2017-08-17 12:39:04 +0100

    Update of war

---
+++ copy-build.bat
@@ -0,0 +1,6 @@
+copy C:\Users\Graduate\Documents\NetBeansProjects\DemoWebApplication\dist\dbankwebtier-c3-1.war C:\tmp\deployment_folder\dist
+cd C:\tmp\deployment_folder
+git add .
+git comment -m "Push new build"
+git push -u origin master
+
